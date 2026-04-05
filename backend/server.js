4bfddd97f0c960f16100b597ddb9b8bccbb2cac5
Rohan Singh <rohansinghiitian6395@gmail.com>
2026-04-05 22:59:55 +0530

    add CLIENT_URL env

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 //middleware to handle cors
 app.use(
     cors({
-        origin: process.env.CLIENT_URL || "*",
+        origin: process.env.CLIENT_URL || '*',
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
